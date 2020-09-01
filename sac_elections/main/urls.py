3d7718c1d83437e951aa93142f1f65c8b1601cc1
Saiakash Konidena <saiakash.konidena@gmail.com>
2020-09-02 02:35:10 +0530

    Added Graph API boilerplate

---
+++ sac_elections/main/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+  # /
+  path('', views.home, name='home'),
+  # TEMPORARY
+  path('signin', views.sign_in, name='signin'),
+  path('signout', views.sign_out, name='signout'),
+  path('callback', views.callback, name='callback'),
+  path('calendar', views.calendar, name='calendar'),
+]
