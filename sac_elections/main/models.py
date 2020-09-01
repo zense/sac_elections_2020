daf34324631fd75f9249d18bb2826425e86ed649
gr455 <ruturaj.mohite@iiitb.org>
2020-09-02 00:32:31 +0530

    feat: models

---
+++ sac_elections/main/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.utils import timezone
+
+def validate_email(email_value):
+
+	VALID_DOMAIN = [
+		'iiitb.org',
+		'iiitb.ac.in',
+	]
+
+	email_domain = email_value[ email_value.find('@')+1:]
+
+	if email_domain not in VALID_DOMAIN:
+		raise ValidationError(
+				_( str(email_domain) + " is not a valid domain."),
+		)
+class User(models.Model):
+	batch = models.CharField(max_length = 10) # ex. 'IMT, MT...'
+	userame = models.CharField(max_length = 255, unique = True) # ex. 'IMT2020518 John Doe'
+	email = models.EmailField(max_length = 255, unique = True, validators = [validate_email])
+	isCandidate = models.BooleanField(default = False)
+
+
+	def __str__(self):
+		return self.username
+
+class Vote(models.Model):
+	candidate = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "candidate") # candidate voted
+	voter = models.OneToOneField(User, unique = True, on_delete = models.RESTRICT, related_name = "voter") # Dont let the profile be deleted if they cast a vote
+	time = models.DateTimeField("voted on", default = timezone.now)
+
+	def __str__(self):
+		return f"Vote by {voter} to {voted_candidate}"
--- sac_elections/sac_elections/settings.py
+++ sac_elections/sac_elections/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
