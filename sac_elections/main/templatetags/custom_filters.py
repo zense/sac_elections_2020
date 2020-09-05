ff6fb31d5889cbf4099564936eee5892b249f0a3
Saiakash Konidena <saiakash.konidena@gmail.com>
2020-09-06 03:59:40 +0530

    Added custom filters for Django templates

---
+++ sac_elections/main/templatetags/custom_filters.py
@@ -0,0 +1,24 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+#custom filters
+#https://docs.djangoproject.com/en/3.1/howto/custom-template-tags/#template-filters-that-expect-strings
+
+register = template.Library()
+
+@register.filter(name="disp")
+@stringfilter
+def getCategoryDisplayNames(value):
+
+  DISPLAY_NAMES = {
+    'MT2020M1': "Mtech Male SAC 1",
+    'MT2020M2': "Mtech Male SAC 2",
+    'MT2020F1': "Mtech Female SAC 2",
+    'MT2020F2': "Mtech Female SAC 2",
+    'IMT2019M1': "iMtech 2019 Male SAC",
+    'IMT2019F1': "iMtech 2019 Female SAC",
+    'IMT2018M1': "iMtech 2018 Male SAC",
+    'IMT2018F1': "iMtech 2018 Female SAC",
+  }
+  
+  return DISPLAY_NAMES[value]
