6dc288e21775aaebc3a0b52a449f87e8fcead788
Ruturaj Mohite <ruturaj.mohite@iiitb.org>
2020-09-13 23:08:35 +0530

    Remove dead files and add nginx config example

--- dbcreation.sql
+++
@@ -1,6 +0,0 @@
-# IMP: Make sure the container starts up on 172.17.0.3
-# IMP: Change pass on production
-
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'172.17.0.3' IDENTIFIED BY 'devPass123' WITH GRANT OPTION;
-SET PASSWORD FOR root@'172.17.0.3' = PASSWORD('root');
-FLUSH PRIVILEGES;
